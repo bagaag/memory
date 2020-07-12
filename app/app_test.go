61d43d1727a4022969b9a37aca8d6b70f72d7ee7
Matt Wiseley <wiseley@gmail.com>
2020-07-12 07:48:00 -0400

    Update for different return type

--- app/app_test.go
+++ app/app_test.go
@@ -39,39 +39,39 @@ func generateData() {
 func TestGetEntries(t *testing.T) {
 	generateData()
 	// defaults
-	notes := GetEntries(EntryTypes{Note: true}, "", "", "", []string{}, 0, 0)
-	if len(notes) != 50 {
-		t.Errorf("Expected 50 entries, got %d", len(notes))
+	results := GetEntries(EntryTypes{Note: true}, "", "", "", []string{}, 0, 0)
+	if len(results.Entries) != 50 {
+		t.Errorf("Expected 50 entries, got %d", len(results.Entries))
 		return
 	}
-	if notes[9].Name() != "note #41" {
-		t.Errorf("Expected 'note #41', got '%s'", notes[9].Name())
+	if results.Entries[9].Name() != "note #41" {
+		t.Errorf("Expected 'note #41', got '%s'", results.Entries[9].Name())
 		return
 	}
 	// no types selected
-	notes = GetEntries(EntryTypes{}, "", "", "", []string{}, 0, 0)
-	if len(notes) != 0 {
-		t.Errorf("Expected 0 entries, got %d", len(notes))
+	results = GetEntries(EntryTypes{}, "", "", "", []string{}, 0, 0)
+	if len(results.Entries) != 0 {
+		t.Errorf("Expected 0 entries, got %d", len(results.Entries))
 		return
 	}
 	// filter by 1 tag and sort by name
-	notes = GetEntries(EntryTypes{Note: true}, "", "", "", []string{"odd"}, SortName, 50)
-	if len(notes) != 25 {
-		t.Errorf("Expected 25 entries, got %d", len(notes))
+	results = GetEntries(EntryTypes{Note: true}, "", "", "", []string{"odd"}, SortName, 50)
+	if len(results.Entries) != 25 {
+		t.Errorf("Expected 25 entries, got %d", len(results.Entries))
 		return
 	}
-	if notes[1].Name() != "note #11" {
-		t.Errorf("Expected 'note #11', got '%s'", notes[1].Name())
+	if results.Entries[1].Name() != "note #11" {
+		t.Errorf("Expected 'note #11', got '%s'", results.Entries[1].Name())
 		return
 	}
 	// filter by 2 tags, sort recent, limit 5
-	notes = GetEntries(EntryTypes{Note: true}, "", "", "", []string{"odd", "bythree"}, SortRecent, 5)
-	if len(notes) != 5 {
-		t.Errorf("Expected 5 entries, got %d", len(notes))
+	results = GetEntries(EntryTypes{Note: true}, "", "", "", []string{"odd", "bythree"}, SortRecent, 5)
+	if len(results.Entries) != 5 {
+		t.Errorf("Expected 5 entries, got %d", len(results.Entries))
 		return
 	}
-	if notes[1].Name() != "note #48" {
-		t.Errorf("Expected 'note #48', got '%s'", notes[1].Name())
+	if results.Entries[1].Name() != "note #48" {
+		t.Errorf("Expected 'note #48', got '%s'", results.Entries[1].Name())
 		return
 	}
 }
