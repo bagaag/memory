deef7c973cefcc208a98558d3805983c554d14c3
Matt Wiseley <wiseley@gmail.com>
2020-07-24 20:29:48 -0400

    Add additional validation rules to entry names

--- app/app.go
+++ app/app.go
@@ -252,6 +252,27 @@ func ValidateEntryName(name string) error {
 	if len(name) == 0 {
 		return errors.New("name cannot be an empty string")
 	}
+	if strings.HasPrefix(name, " ") {
+		return errors.New("name cannot start with a space")
+	}
+	if strings.HasSuffix(name, " ") {
+		return errors.New("name cannot end with a space")
+	}
+	if strings.Contains(name, "\n") || strings.Contains(name, "\r") {
+		return errors.New("name cannot contain line breaks")
+	}
+	if strings.Contains(name, "\t") {
+		return errors.New("name cannot contain tab characters")
+	}
+	if strings.Contains(name, "  ") {
+		return errors.New("name cannot more than 1 consecutive space")
+	}
+	if strings.HasPrefix(name, "!") {
+		return errors.New("name cannot start with a ! character")
+	}
+	if strings.Contains(name, "[") || strings.Contains(name, "]") {
+		return errors.New("name cannot contain [ or ]")
+	}
 	if len(name) > config.MaxNameLen {
 		return fmt.Errorf("name length cannot exceed %d", config.MaxNameLen)
 	}
